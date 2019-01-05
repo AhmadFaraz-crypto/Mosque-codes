a743d7363d225d1a519097237b67e9b66fdd711b
farazahmadjutt <farazkh.bh@gmail.com>
2019-01-05 03:43:19 -0800

    Soucecode Mosque program

---
+++ Sourcecode.cpp
@@ -0,0 +1,241 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int i=0;i<=5;i++)
+	{
+		cout<<"\t\t";
+		for(int j=0;j<=5-i;j++)cout<<" ";
+		for(int k=0;k<=2*i;k++)cout<<"#";
+		for(int j=0;j<97;j++)cout<<" ";
+		for(int j=0;j<=5-i;j++)cout<<"  ";
+		for(int k=0;k<=2*i;k++)cout<<"#";
+		cout<<"\n";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\t\t ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<99;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<99;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<99;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<99;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t       ";
+		for(int j=0;j<15;j++)cout<<"*";
+		for(int j=0;j<95;j++)cout<<" ";
+		for(int j=0;j<15;j++)cout<<"*";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t";
+		for(int j=0;j<13;j++)cout<<"*";
+		for(int j=0;j<97;j++)cout<<" ";
+		for(int j=0;j<13;j++)cout<<"*";
+	}
+	for(int i=0;i<6;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<=15-i;j++)cout<<" ";
+		for(int k=0;k<=2*i;k++)cout<<"#";
+		for(int j=0;j<55;j++)cout<<" ";
+		for(int j=0;j<=5-i;j++)cout<<"  ";
+		for(int k=0;k<=2*i;k++)cout<<"#";
+		for(int j=0;j<14-i;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t       ";
+		for(int j=0;j<15;j++)cout<<"*";
+		for(int j=0;j<9;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<57;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<7;j++)cout<<" ";
+		for(int j=0;j<15;j++)cout<<"*";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t";
+		for(int j=0;j<13;j++)cout<<"*";
+		for(int j=0;j<10;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<57;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<8;j++)cout<<" ";
+		for(int j=0;j<13;j++)cout<<"*";
+	}
+	for(int i=0;i<2;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<11;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<57;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<9;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<9;j++)cout<<" ";
+		for(int j=0;j<15;j++)cout<<"*";
+		for(int j=0;j<53;j++)cout<<" ";
+		for(int j=0;j<15;j++)cout<<"*";
+		for(int j=0;j<7;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<10;j++)cout<<" ";
+		for(int j=0;j<13;j++)cout<<"*";
+		for(int j=0;j<55;j++)cout<<" ";
+		for(int j=0;j<13;j++)cout<<"*";
+		for(int j=0;j<8;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+	}
+	for(int i=0;i<4;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<11;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<57;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+		for(int j=0;j<9;j++)cout<<" ";
+		for(int j=0;j<11;j++)cout<<"|";
+	}
+	for(int i=0;i<3;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<121;j++)cout<<"#";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<27;j++)cout<<"#";
+		for(int j=0;j<2;j++)cout<<"*";
+		for(int j=0;j<33;j++)cout<<"#";
+		for(int j=0;j<2;j++)cout<<"*";
+		for(int j=0;j<31;j++)cout<<"#";
+		for(int j=0;j<2;j++)cout<<"*";
+		for(int j=0;j<24;j++)cout<<"#";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<26;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<2;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<31;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<2;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<29;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<2;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<23;j++)cout<<"#";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<25;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<4;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<29;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<4;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<27;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<4;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<22;j++)cout<<"#";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<24;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<6;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<27;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<6;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<25;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<6;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<21;j++)cout<<"#";
+	}
+	for(int i=0;i<1;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<23;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<8;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<25;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<8;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<23;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<8;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<20;j++)cout<<"#";
+	}
+	for(int i=0;i<2;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<121;j++)cout<<"|";
+	}
+	for(int i=0;i<10;i++)
+	{
+		cout<<"\n\t\t ";
+		for(int j=0;j<23;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<8;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<25;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<8;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<23;j++)cout<<"#";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<8;j++)cout<<" ";
+		for(int j=0;j<1;j++)cout<<"*";
+		for(int j=0;j<20;j++)cout<<"#";
+	}
+}
